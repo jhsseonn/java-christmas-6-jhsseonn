10db086043c28b01c192e84d4e5c36aa20b7e799
jhsseonn <hs125bt@gmail.com>
2023-11-14 00:34:22 +0900

    feat: 이벤트 시작 문구 출력 및 예상 방문날짜 입력받기

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,16 @@
 package christmas;
 
+import christmas.view.ChristmasInputView;
+import christmas.view.ChristmasOutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        final ChristmasInputView christmasInputView = new ChristmasInputView();
+        final ChristmasOutputView christmasOutputView = new ChristmasOutputView();
+
+        christmasOutputView.printWelcome();
+        int expectedVisitDay = christmasInputView.getExpectedVisitDay();
+
     }
 }
