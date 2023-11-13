b7ae4443e18767c684f3d2da158ccad7db63b33e
jhsseonn <hs125bt@gmail.com>
2023-11-14 01:17:01 +0900

    feat: 크리스마스 프로모션 종류 enum 생성

---
+++ src/main/java/christmas/domain/ChristmasPromotionEvents.java
@@ -0,0 +1,9 @@
+package christmas.domain;
+
+public enum ChristmasPromotionEvents {
+    CHRISTMAS_DDAY_PROMOTION,
+    WEEKDAY_PROMOTION,
+    WEEKEND_PROMOTION,
+    SPECIAL_PROMOTION,
+    PRESENTATION_PROMOTION
+}
