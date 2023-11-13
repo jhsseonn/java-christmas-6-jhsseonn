e22fe5b0b1340448f0972255cc584b3b70ac1591
jhsseonn <hs125bt@gmail.com>
2023-11-13 21:13:22 +0900

    feat: 12월 이벤트 배지 enum 생성

--- src/main/java/christmas/consts/ChristmasConsts.java
+++ src/main/java/christmas/consts/ChristmasConsts.java
@@ -9,23 +9,24 @@ public interface ChristmasConsts {
         put("타파스",5500);
         put("시저샐러드",8000);
     }};
-
     HashMap<String, Integer> MAINS = new HashMap<>(){{
         put("티본스테이크",55000);
         put("바비큐립",54000);
         put("해산물파스타",35000);
         put("크리스마스파스타",25000);
     }};
-
     HashMap<String, Integer> DESSERTS = new HashMap<>(){{
         put("초코케이크",15000);
         put("아이스크림",5000);
     }};
-
     HashMap<String, Integer> BEVERAGES = new HashMap<>(){{
         put("제로콜라",3000);
         put("레드와인",60000);
         put("샴페인",25000);
     }};
+    int DECEMBER_EVENT_BADGE_STAR_AMOUNT = 5000;
+    int DECEMBER_EVENT_BADGE_TREE_AMOUNT = 10000;
+    int DECEMBER_EVENT_BADGE_SANTA_AMOUNT = 20000;
+
 
 }
---
+++ src/main/java/christmas/domain/DecemberEventBadge.java
@@ -0,0 +1,15 @@
+package christmas.domain;
+
+import christmas.consts.ChristmasConsts;
+
+public enum DecemberEventBadge implements ChristmasConsts {
+    STAR(DECEMBER_EVENT_BADGE_STAR_AMOUNT),
+    TREE(DECEMBER_EVENT_BADGE_TREE_AMOUNT),
+    SANTA(DECEMBER_EVENT_BADGE_SANTA_AMOUNT);
+
+    private int totalAdvantageAmount;
+
+    DecemberEventBadge(int totalAdvantageAmount){
+        this.totalAdvantageAmount = totalAdvantageAmount;
+    }
+}
