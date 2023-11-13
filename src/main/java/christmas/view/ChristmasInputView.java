f05a61ca5d73a294f8f4920289bc49b16c82b17f
jhsseonn <hs125bt@gmail.com>
2023-11-13 21:33:58 +0900

    feat: 12월 중 식당 예상 방문 날짜 입력받기

--- src/main/java/christmas/view/ChristmasInputView.java
+++ src/main/java/christmas/view/ChristmasInputView.java
@@ -1,4 +1,11 @@
 package christmas.view;
 
-public class ChristmasInputView {
+import camp.nextstep.edu.missionutils.Console;
+import christmas.consts.ChristmasConsts;
+
+public class ChristmasInputView implements ChristmasConsts {
+    public int getExpectedVisitDay(){
+        System.out.println(DECEMBER_EVENT_EXPECTED_VISIT_DAY);
+        return Integer.parseInt(Console.readLine());
+    }
 }
