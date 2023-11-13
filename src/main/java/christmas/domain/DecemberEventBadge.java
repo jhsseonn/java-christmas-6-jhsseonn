d2a5792aac1a68180a8f6f1f4ee11626d2c63584
jhsseonn <hs125bt@gmail.com>
2023-11-13 21:14:54 +0900

    feat: 12월 이벤트 배지 중 배지를 받지 못하는 경우 추가

--- src/main/java/christmas/domain/DecemberEventBadge.java
+++ src/main/java/christmas/domain/DecemberEventBadge.java
@@ -5,7 +5,8 @@ import christmas.consts.ChristmasConsts;
 public enum DecemberEventBadge implements ChristmasConsts {
     STAR(DECEMBER_EVENT_BADGE_STAR_AMOUNT),
     TREE(DECEMBER_EVENT_BADGE_TREE_AMOUNT),
-    SANTA(DECEMBER_EVENT_BADGE_SANTA_AMOUNT);
+    SANTA(DECEMBER_EVENT_BADGE_SANTA_AMOUNT),
+    NONE(DECEMBER_EVENT_BADGE_NONE_AMOUNT);
 
     private int totalAdvantageAmount;
 
