8d191bce2e977960c1eea05ac0a92f92812d2304
jhsseonn <hs125bt@gmail.com>
2023-11-14 15:36:12 +0900

    test: 메뉴명으로 크리스마스 메뉴 및 메뉴 가격 조회 테스트 통과

---
+++ src/test/java/christmas/consts/ChristmasMenuFixture.java
@@ -0,0 +1,6 @@
+package christmas.consts;
+
+public class ChristmasMenuFixture {
+    final String 유효한_메뉴 = "티본스테이크";
+    final int 유효한_메뉴_가격 = 55000;
+}
---
+++ src/test/java/christmas/consts/ChristmasMenuTest.java
@@ -0,0 +1,32 @@
+package christmas.consts;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ChristmasMenuTest extends ChristmasMenuFixture{
+
+    @Test
+    void 메뉴명으로_크리스마스_메뉴_조회() {
+        ChristmasMenu result = ChristmasMenu.findByMenu(유효한_메뉴);
+        assertThat(result).isEqualTo(ChristmasMenu.MAIN);
+    }
+
+    @Test
+    void 메뉴명으로_크리스마스_메뉴_가격_조회() {
+        ChristmasMenu christmasMenu = ChristmasMenu.findByMenu(유효한_메뉴);
+        HashMap<String, Integer> getChristmasMenu = christmasMenu.getMenu();
+        int result = getChristmasMenu.get(유효한_메뉴);
+        assertThat(result).isEqualTo(유효한_메뉴_가격);
+    }
+
+    @Test
+    void getMenu() {
+    }
+
+    @Test
+    void getMenuPrice() {
+    }
+}
