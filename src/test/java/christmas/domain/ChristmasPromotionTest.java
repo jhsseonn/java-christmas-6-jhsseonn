61fc87e470774c2f6d99727ff69cef03d295f20a
jhsseonn <hs125bt@gmail.com>
2023-11-15 01:43:59 +0900

    test: 평일 할인 테스트 통과

--- src/test/java/christmas/domain/ChristmasPromotionTest.java
+++ src/test/java/christmas/domain/ChristmasPromotionTest.java
@@ -1,16 +1,30 @@
 package christmas.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ChristmasPromotionTest {
 
+    public Order orderHistory;
+    public ChristmasPromotion christmasPromotion;
+    @BeforeEach
+    void 유효한_order_생성(){
+        orderHistory = new Order(3, "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1");
+        christmasPromotion = new ChristmasPromotion(orderHistory);
+    }
     @Test
     void 크리스마스_디데이_이벤트_추가() {
-        Order orderHistory = new Order(3, "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1");
-        ChristmasPromotion christmasPromotion = new ChristmasPromotion(orderHistory);
-        christmasPromotion.addChristmasDDayEvent(orderHistory.getOrderDate());
+        int ddayPromotionAmount = christmasPromotion.addChristmasDDayEvent(orderHistory.getOrderDate());
+        assertThat(ddayPromotionAmount).isEqualTo(1200);
         assertThat(christmasPromotion.getTotalPromotionAmount()).isEqualTo(1200);
+    }
+
+    @Test
+    void 평일_할인_이벤트_추가(){
+        int weekdayEventAmount = christmasPromotion.addWeekDayEvent(orderHistory);
+        assertThat(weekdayEventAmount).isEqualTo(4046);
+        assertThat(christmasPromotion.getTotalPromotionAmount()).isEqualTo(4046);
     }
 }
