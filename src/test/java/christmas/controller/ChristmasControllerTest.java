7716eefc3e247cf290be9f3fb7a05021367a1414
jhsseonn <hs125bt@gmail.com>
2023-11-15 22:11:47 +0900

    test: 주문 메뉴에 음료만 있는 경우 예외처리 테스트 통과

---
+++ src/test/java/christmas/controller/ChristmasControllerTest.java
@@ -0,0 +1,34 @@
+package christmas.controller;
+
+import org.junit.jupiter.api.Test;
+
+class ChristmasControllerTest {
+
+    private ChristmasController christmasController = new ChristmasController();
+
+    @Test
+    void getValidVisitDay() {
+
+    }
+
+    @Test
+    void isValidRangeVisitDay() {
+    }
+
+    @Test
+    void getOrderMenus() {
+    }
+
+    @Test
+    void computeTotalOrderCount() {
+
+    }
+
+    @Test
+    void getValidMenuCount() {
+    }
+
+    @Test
+    void getExpectVisitDate() {
+    }
+}
