844228ef754ab47dd441cb9e10b98d4a7557b4eb
jhsseonn <hs125bt@gmail.com>
2023-11-15 14:36:34 +0900

    feat: 혜택 내역 문구 출력

--- src/main/java/christmas/view/ChristmasOutputView.java
+++ src/main/java/christmas/view/ChristmasOutputView.java
@@ -14,34 +14,41 @@ public class ChristmasOutputView implements ChristmasConsts {
     public void printOrderMenus(HashMap<String, Integer> orderMenuPreview){
         System.out.println("<주문 메뉴>");
         orderMenuPreview.forEach((key, value) -> {
-            System.out.printf("%s %d개", key, value);
+            System.out.printf("%s %d개\n", key, value);
         });
+        System.out.println();
     }
 
     public void printTotalOrderAmount(int totalOrderAmount){
         System.out.println("<할인 전 총주문 금액>");
-        System.out.printf("%d원", totalOrderAmount);
+        System.out.printf("%d원\n", totalOrderAmount);
+        System.out.println();
     }
 
     public void printPresentationEventMenu(){
         System.out.println("<증정 메뉴>");
-        System.out.printf("%s %d개", "샴페인", 1);
+        System.out.printf("%s %d개\n", "샴페인", 1);
+        System.out.println();
     }
 
     public void printPromotionResult(HashMap<String, Integer> promotionResult){
+        System.out.println("<혜택 내역>");
         promotionResult.forEach((key, value) -> {
-            System.out.printf("%s: -%d원", key, value);
+            System.out.printf("%s: -%d원\n", key, value);
         });
+        System.out.println();
     }
 
     public void printTotalPromotionAmount(int totalPromotionAmount){
         System.out.println("<총혜택 금액>");
-        System.out.printf("-%d", totalPromotionAmount);
+        System.out.printf("-%d\n", totalPromotionAmount);
+        System.out.println();
     }
 
     public void printExpectAmountAfterDiscount(int expectAmountAfterDiscount){
         System.out.println("<할인 후 예상 결제 금액>");
-        System.out.printf("%d", expectAmountAfterDiscount);
+        System.out.printf("%d\n", expectAmountAfterDiscount);
+        System.out.println();
     }
 
     public void printDecemberEventBadge(String decemberEventBadge){
