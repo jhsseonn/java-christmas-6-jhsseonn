160e95a7e7295351d3d102c79d5c2a6f74ae100e
jhsseonn <hs125bt@gmail.com>
2023-11-15 16:25:24 +0900

    refactor: 유효하지 않은 날짜 리팩토링

--- src/main/java/christmas/exception/GlobalExceptionHandler.java
+++ src/main/java/christmas/exception/GlobalExceptionHandler.java
@@ -1,11 +1,7 @@
 package christmas.exception;
 
-public class GlobalExceptionHandler {
-    public void isInputInteger(String input, String errorMessage){
-        try{
-            Integer.parseInt(input);
-        } catch (NumberFormatException e){
-            System.out.printf("%s %s", "[ERROR]", errorMessage);
-        }
-    }
+import christmas.consts.ChristmasConsts;
+
+public class GlobalExceptionHandler implements ChristmasConsts {
+
 }
